dc13292e5c1e46633b67cf70dfbf2b750c393d31
Fernando Vidal <45366784+lfvidal@users.noreply.github.com>
2025-01-27 16:32:28 -0500

    Add files via upload

---
+++ Python_course/hello.py
@@ -0,0 +1,25 @@
+print("hello world")
+
+picture = [
+    [0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 1, 1, 1, 1, 1, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 1, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0],
+]
+
+
+def show_umbrella():
+    for row in picture:
+        initialization = ""
+        for value in row:
+            if value == 1:
+                initialization += "*"
+            else:
+                initialization += " "
+        print(initialization)
+
+
+show_umbrella()
+
---
+++ Python_course/py.py
@@ -0,0 +1,28 @@
+"""
+This notebook demonstrates how to create a basic class in Python and work with object types.
+"""
+
+
+class BigObject:
+    """
+    This is a empty class created as an example to demonstrate the structure of a class in Python.
+    """
+
+    pass
+
+
+# create instances of the class
+obj1 = BigObject()  # object or Instantiate
+obj2 = BigObject()  # object or Instantiate
+obj3 = BigObject()  # object or Instantiate
+
+# Demonstrate the type of the object
+print(type(None))
+print(type(True))
+print(type(5))
+print(type(5.5))
+print(type("hi"))
+print(type([]))
+print(type(()))
+print(type({}))
+print(type(obj1))
