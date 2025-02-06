761bb9e508a476181a236cae5b8f85387ec3e50a
Fernando Vidal <luisfer048@gmail.com>
2025-02-06 18:36:18 -0500

    test created

--- Python_course/Modules/main.py
+++ Python_course/Modules/main.py
@@ -1,16 +1,15 @@
-# from utility import *
-import utility
+from utility import *
 
-from shopping.shopping_cart import buy
 
-# print(multiply(2, 3))
-# print(divide(6, 3))
-# print(buy('apple'))
-# print(max_custom())
-
+# from shopping.shopping_cart import buy
 if __name__ == '__main__':
     print('This is the main file')
+    print(multiply(2, 3))
+    print(divide(6, 3))
+    print(buy('apple'))
+    print(max_custom())
 
-print(type(utility.st1))
+
+
 
 
